dcd62ee6a7e30f71d1c5546509fc372c219ac135
GitHub Action <action@github.com>
2022-07-18 13:13:00 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fengdh23/intellijplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fengdh23.intellijplugintemplate.services
+
+import com.github.fengdh23.intellijplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/fengdh23/intellijplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fengdh23/intellijplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fengdh23.intellijplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fengdh23.intellijplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
